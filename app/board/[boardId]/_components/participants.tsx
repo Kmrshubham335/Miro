0b449b1182800b496d9238128ececffa96bb46fa
Kmrshubham335 <kumarshubham09133@gmail.com>
2024-03-23 20:02:53 +0530

    Canvas Layout completed

---
+++ app/board/[boardId]/_components/participants.tsx
@@ -0,0 +1,7 @@
+export const Participants=()=>{
+    return (
+        <div className="absolute h-12 t-2 right-2 bg-white rounded-md p-3 flex item-center shadow-md" >
+            User List
+        </div>
+    )
+}
